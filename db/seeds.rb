27e2f3213651647f4cdcdd688e2737ed3663d473
Mark J. Reed <mark.reed@turner.com>
2014-10-25 13:10:26 -0400

    formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-["Artifact Fragment", "Biochemical Compound", "Biochemical Sample", 
- "Color Crystal", "Gemstone", "Lockbox", "Luxury Fabric", "Medical Supply", 
- "Power Crystal", "Researched Compound", "Scavenged Compound", "Scavenged Metal", 
- "Underworld Metal"].each do |name|
+[ "Artifact Fragment",    "Biochemical Compound", "Biochemical Sample", 
+  "Color Crystal",        "Gemstone",             "Lockbox",             
+  "Luxury Fabric",        "Medical Supply",       "Power Crystal",     
+  "Researched Compound",  "Scavenged Compound",   "Scavenged Metal", 
+  "Underworld Metal" ].each do |name|
    MaterialType.create!(name: name)
 end
