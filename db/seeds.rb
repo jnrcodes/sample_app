0cb642a2c6b0c24074f421c3731ec4f7daedb389
Mark J. Reed <mark.reed@turner.com>
2014-10-25 13:06:35 -0400

    clean schema, add material type seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@
  "Color Crystal", "Gemstone", "Lockbox", "Luxury Fabric", "Medical Supply", 
  "Power Crystal", "Researched Compound", "Scavenged Compound", "Scavenged Metal", 
  "Underworld Metal"].each do |name|
-   MaterialType.create(name: name)
+   MaterialType.create!(name: name)
 end
